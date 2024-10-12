dc98505b7f1cfcbd3687d5d0a5dfbc9c8e966d3c
Sashavav <vav110011@gmail.com>
2024-10-12 14:45:08 +0300

    Initial commit

---
+++ PyTorch_course/Seminars/seminar1/1/__init__.py
@@ -0,0 +1,22 @@
+# TODO
+#  Linux install
+#  Anaconda
+#  python 3.10 env&
+#  conda install
+#  pep 8 read
+#  google python style guide
+#  "black" formatter
+#  pre-commit
+#  type function
+#  mypy
+#  -----------------------
+#  Homework: Project
+#  Git:
+#   git flow (branches Main, hotfix, develop, feature)
+#   --no-ff
+#  Poetry with Numpy using typification
+#   poetry config --list
+#   v.create = false
+#  Anaconda
+#  pre-commit
+#  black
