bc4e945961c0f8b91466a29d89f2ad278f23283f
Sashavav <vav110011@gmail.com>
2024-10-13 21:15:38 +0300

    Small update

--- PyTorch_course/Homeworks/Task1/task1/__init__.py
+++ PyTorch_course/Homeworks/Task1/task1/__init__.py
@@ -2,6 +2,5 @@ def my_sum(my_a: float, my_b: float) -> float:
     return my_a + my_b
 
 
-print("Hello World")
-a, b = map(int, input("Enter a and b").split())
+a, b = map(float, input("Enter a and b\n").split())
 print(f"Sum of a and b = {a+b}")
