eb7487eb2483d9b350099c3b181c1d9f56914900
Sashavav <vav110011@gmail.com>
2024-11-22 21:21:26 +0300

    Add CNN MNIST, Accuracy: 98.92

---
+++ Models/CNN/MNIST/test/test.py
@@ -0,0 +1,27 @@
+import torch
+from model import cnn
+import torch.nn as nn
+from data import test_data_loader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+criterion = nn.CrossEntropyLoss()
+cnn.to(device=device)
+cnn.load_state_dict(torch.load("cnn.pth", weights_only=True))
+
+
+def main():
+    correct = 0
+    total = 0
+    for i, data in enumerate(test_data_loader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = cnn(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print(f"accuracy: {correct * 100 / total}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Models/CNN/MNIST/train/train.py
@@ -0,0 +1,44 @@
+import torch
+
+from model import cnn
+from data import train_data_loader, test_data_loader
+import torch.nn as nn
+import torch.optim as optim
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+criterion = nn.CrossEntropyLoss()
+cnn.to(device=device)
+optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
+
+cnn.load_state_dict(torch.load("cnn.pth", weights_only=True))
+
+
+def main():
+    for epoch in range(20):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_data_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = cnn(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+                running_loss = 0.0
+                torch.save(cnn.state_dict(), "cnn.pth")
+
+    print("Finished Training")
+
+
+if __name__ == "__main__":
+    main()
